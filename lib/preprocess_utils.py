7b94de38ef18df45d16bf4ab3b0b5fdad1528444
Медведев Игорь Сергеевич <farshov@MacBook-Pro-Medvedev.local>
2019-10-04 20:43:53 +0300

    work with date in users dataframe is done

--- lib/preprocess_utils.py
+++ lib/preprocess_utils.py
@@ -1,16 +1,18 @@
 import numpy as np
+from datetime import datetime
 
 
-def calc_duration(data):
+def calc_duration(data, nan='NAN'):
     """
     Claculating of duration of using of Tinkoff app in years
     :param data: dataframe containing column first_session_dttm
+           nan: format of Nanin your dataframe
     :return: None, adds column of duration in-place
     """
     new_col = [0] * len(data)
     for i in range(len(data)):
         date = data.first_session_dttm[i]
-        if date != 'NAN':
+        if date != nan:
             duration = (datetime.today() - datetime.strptime(data.first_session_dttm[i],
                                                              '%Y-%m-%d %H:%M:%S')).days / 365
             new_col[i] = duration
@@ -18,7 +20,22 @@ def calc_duration(data):
     na = np.average(new_col)
     for i in range(len(data)):
         date = data.first_session_dttm[i]
-        if date == 'NAN':
+        if date == nan:
             duration = na
             new_col[i] = duration
     data['duration'] = new_col
+
+
+def transform_to_year(data, nan='NAN'):
+    """
+        Transforming datetime into just year
+        :param data: dataframe containing column first_session_dttm
+               nan: format of Nanin your dataframe
+        :return: None, adds column of duration in-place
+        """
+    years = [nan] * len(data)
+    for i in range(len(data)):
+        date = data.first_session_dttm[i]
+        if date != nan and isinstance(date, str):
+            years[i] = datetime.strptime(data.first_session_dttm[i], '%Y-%m-%d %H:%M:%S').year
+    data.first_session_dttm = years
