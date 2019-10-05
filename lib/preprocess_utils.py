394702de57f9b3bfa246337d2b109747ab947a92
Медведев Игорь Сергеевич <farshov@MacBook-Pro-Medvedev.local>
2019-10-05 14:18:30 +0300

    preprocessing of transactions

--- lib/preprocess_utils.py
+++ lib/preprocess_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from datetime import datetime
+import pandas as pd
 
 
 def calc_duration(data, nan='NAN'):
@@ -28,14 +29,44 @@ def calc_duration(data, nan='NAN'):
 
 def transform_to_year(data, nan='NAN'):
     """
-        Transforming datetime into just year
-        :param data: dataframe containing column first_session_dttm
-               nan: format of Nanin your dataframe
-        :return: None, adds column of duration in-place
-        """
+    Transforming datetime into just year
+    :param data: dataframe containing column first_session_dttm
+           nan: format of Nanin your dataframe
+    :return: None, adds column of duration in-place
+    """
     years = [nan] * len(data)
     for i in range(len(data)):
         date = data.first_session_dttm[i]
         if date != nan and isinstance(date, str):
             years[i] = datetime.strptime(data.first_session_dttm[i], '%Y-%m-%d %H:%M:%S').year
     data.first_session_dttm = years
+
+
+def aggregate_transactions(path):
+    transactions = pd.read_csv(path)
+    transactions.index = transactions.customer_id
+    transaction = transactions.drop(columns='customer_id')
+    av = []
+    av_n_z = []
+    num_tr = []
+    num_tr_n_z = []
+    idxs = np.unique(transaction.index)
+    for i in idxs:
+        data = transaction.loc[i]
+        if type(data) != pd.core.series.Series:
+            num_tr.append(len(data.transaction_amt))
+            av.append(np.mean(data.transaction_amt))
+            if sum(data.transaction_amt != 0) > 0:
+                num_tr_n_z.append(sum(data.transaction_amt[data.transaction_amt != 0]))
+                av_n_z.append(np.mean(data.transaction_amt[data.transaction_amt != 0]))
+            else:
+                num_tr_n_z.append(1.)
+                av_n_z.append(0.)
+        else:
+            num_tr.append(1.)
+            num_tr_n_z.append(1.)
+            av.append(0.)
+            av_n_z.append(0.)
+    tr_data = np.array([av, av_n_z, num_tr, num_tr_n_z]).T
+    tr_data = pd.DataFrame(tr_data, index=idxs, columns=['av', 'av_n_z', 'num_tr', 'num_tr_n_z'])
+    return tr_data
