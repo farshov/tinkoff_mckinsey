c81d8273dec7dbc68b0f111f932f61649200e2a7
farshov <medvedev.is@phystech.edu>
2019-10-06 14:01:29 +0300

    embeddings are done

--- lib/utils.py
+++ lib/utils.py
@@ -1,19 +1,30 @@
 import string
+import re
 
 
 def clean_sentence(s):
-    symb = "!@#%*&()[]{}/?²\"\'#№.,:;%*()<>\n₽1234567890-+–«»\"\\"
-    for c in symb:
-        s = s.replace(c, "")
-    s = s.replace('\xa0', " ")
+    # symb = "!@#%*&()[]{}/?²\"\'#№.,:;%*()<>\n₽1234567890-+–«»\"\\qwertyuiopasdfghjklzxcvbnm"
+    # symb = "йцукенгшщзхъфывапролджэёячсмитьбю"
+    s = s.lower()
+    #for c in symb:
+        #s = s.replace(c, "")
+    s = re.sub('[^йцукенгшщзхъфывапролджэёячсмитьбю]', ' ', s)
+    # s = s.replace('\xa0', " ")
 
     return s.lower().strip()
-
+    
 
 def clean_text(text):
-    res = ""
+    res = []
+    num_words = 0
     for i in range(len(text)):
         clean = clean_sentence(text[i])
         if len(clean) > 0:
-            res += " " + clean
-    return res.strip(), len(text), len(res.split())
+            clean = clean.split()
+            # no n_grams for single characters
+            for w in clean:
+                if len(w) < 2:
+                    continue
+            res.append(clean)
+            num_words += len(clean)
+    return res, num_words
